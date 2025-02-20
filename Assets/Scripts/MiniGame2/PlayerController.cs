33b241b52ded2f19ddbdf0040a4f4e58b4900a1a
vmffpdlwnd <leegunehee@gmail.com>
2025-02-20 17:33:48 +0900

    Fix: 캐릭터가 이상한곳을 보는 버그 수정

--- Assets/Scripts/MiniGame2/PlayerController.cs
+++ Assets/Scripts/MiniGame2/PlayerController.cs
@@ -8,21 +8,21 @@ namespace MiniGame2
 
        void Update() 
        {
-           // 마우스 위치를 월드 좌표로 변환
-           Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // 마우스 위치를 월드 좌표로 변환
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
            
-           // 캐릭터가 마우스의 x축 위치에 따라 좌우 방향 전환
-           if(mousePosition.x > transform.position.x)
-           {
-               transform.parent.localScale = new Vector3(2f, 2f, 2f); // 오른쪽 보기
-           }
-           else
-           {
+            // 캐릭터가 마우스의 x축 위치에 따라 좌우 방향 전환
+            if(mousePosition.x > transform.position.x)
+            {
                transform.parent.localScale = new Vector3(-2f, 2f, 2f); // 왼쪽 보기
-           }
+            }
+            else
+            {
+                transform.parent.localScale = new Vector3(2f, 2f, 2f); // 오른쪽 보기
+            }
 
-           // 기본 idle 애니메이션 재생
-           spumPrefab._anim.SetBool("Run", false);
+            // 기본 idle 애니메이션 재생
+            spumPrefab._anim.SetBool("Run", false);
        }
    }
 }
